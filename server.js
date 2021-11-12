5469907ca559f2006936b011eb34294c16ef1bd5
Bonishkumar <kshbonish@gmail.com>
2021-11-12 13:19:43 +0530

    Completed

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/Users_api",(req,res)=>{
     res.render("User_interface");
 
 })
-
+////////////////////////////////////////////////////////////////////////
 app.post("/login",async (req,res)=>{
     data.collection("Users_data").findOne({username:req.body.username},async function(err,result){
         if(err) throw err;
@@ -111,8 +111,8 @@ app.post("/signup",(req,res)=>{
     })
   }
 })
-
-app.post("/createroom",(req,res)=>{
+//////////////////////////////////////////////////////////////////////
+app.post("/createroom",(req,res)=>{   
     
     var r_number=req.body.r_number;
     data.collection("room_data").findOne({r_number:r_number},function(err,result){
@@ -123,7 +123,7 @@ app.post("/createroom",(req,res)=>{
         else{
             var seats = req.body.seats;
             var price = req.body.price;
-            var input={r_number:r_number,seats:seats,price:price,status:"Not_booked",c_name:"",c_phone:"",c_email:"",s_datetime:"",e_datetime:"",duration:"",total:""};
+            var input={r_number:r_number,seats:seats,price:price};
             data.collection("room_data").insertOne(input,function(err,result){
                 if (err) throw err;
                 console.log(result);
@@ -141,22 +141,29 @@ app.post("/bookroom",(req,res)=>{
     data.collection("room_data").findOne({r_number:r_number},function(err,result){
         if (err) throw err;
         if(result!=undefined){
-            if(result.status!="Booked"){
-                var total = req.body.duration*result.price;
-                var input = {$set:{c_name:req.body.c_name,c_phone:req.body.c_phone,c_email:req.body.c_email,s_datetime:req.body.s_datetime,
-                            e_datetime:req.body.e_datetime,duration:req.body.duration,total:total,status:"Booked"}};       
-    
-            data.collection("room_data").updateOne({r_number:r_number},input,function(err,result){
-                if (err) throw err;
-                if(result!=undefined){
-                    console.log("Success");
-                    res.render("staff_interface",{message1:"Room sucessfully booked!!!"});
-                }
-            })       
-            }
-            else{
-                res.render("staff_interface",{message2:"Room not available!!!  PLease check room status!!!"})
-            }
+                var total = 12*result.price;
+                var seats = result.seats;
+                var price = result.price;
+                var input = {r_number:r_number,seats:seats,price:price,c_name:req.body.c_name,c_phone:req.body.c_phone,c_email:req.body.c_email,date:req.body.date,total:total,status:"Booked"}; 
+
+                data.collection("room_status").findOne({r_number:r_number,date:req.body.date},function(err,result){
+                    if (err) throw err;
+                    if(result==undefined){
+                        data.collection("room_status").insertOne(input,function(err,result){
+                            if (err) throw err;
+                            if(result!=undefined){
+                                console.log("Success");
+                                res.render("staff_interface",{message1:"Room sucessfully booked!!!"});
+                            }
+                        })      
+                        }
+                        else{
+                            res.render("staff_interface",{message2:"Room already booked!!!  Please check room status!!!"})
+                        }
+            
+                        })
+                    
+               
                      
         }
         else{
@@ -165,11 +172,134 @@ app.post("/bookroom",(req,res)=>{
     })
 })
 
+app.post("/checkroom",(req,res)=>{
+        data.collection("room_data").findOne({seats:req.body.seats},function(err,result){
+            if (err) throw err;
+            var number1 = result.r_number;
+            var price1 = result.price;
+            if(result!=undefined){
+                data.collection("room_status").find({seats:req.body.seats},{ projection: { _id: 0, r_number: 1, date: 1, price: 1 } }).toArray(function(err,result){
+                    if (err) throw err;
+                    if(result.length>0){
+                        var a=0;
+                        console.log(result);
+                       for(let i=0;i<result.length;i++){
+                           
+                           if(result[i].date!=req.body.date){
+                               var number = result[i].r_number;
+                               a=1;
+                                res.render("staff_interface",{message1:"Room number "+number+" is available and is priced at "+result[i].price});
+                           }
+                           
+                       }
+                       if(a==0){
+                           console.log("cannot find 1");
+                        res.render("staff_interface",{message2:"Cannot find matching room number"});
+                       }
+                    }
+                    else{
+                        console.log("Undefined");
+                        res.render("staff_interface",{message1:"Room number "+number1+" is available and is priced at "+price1});
+                    }
+                })
+            }
+            else{
+                console.log("cannot find 2");
+                        res.render("staff_interface",{message2:"Cannot find matching room number"});
+            }
+        })
+})
+//////////////////////////////////////////////////////////////////////
+app.post("/userbookroom",(req,res)=>{
+    var r_number = req.body.r_number;
+    data.collection("room_data").findOne({r_number:r_number},function(err,result){
+        if (err) throw err;
+        if(result!=undefined){
+                var total = 12*result.price;
+                var seats = result.seats;
+                var price = result.price;
+                var input = {r_number:r_number,seats:seats,price:price,c_name:req.body.c_name,c_phone:req.body.c_phone,c_email:req.body.c_email,date:req.body.date,total:total,status:"Booked"}; 
+
+                data.collection("room_status").findOne({r_number:r_number,date:req.body.date},function(err,result){
+                    if (err) throw err;
+                    if(result==undefined){
+                        data.collection("room_status").insertOne(input,function(err,result){
+                            if (err) throw err;
+                            if(result!=undefined){
+                                console.log("Success");
+                                res.render("User_interface",{message1:"Room sucessfully booked!!!"});
+                            }
+                        })      
+                        }
+                        else{
+                            res.render("User_interface",{message2:"Room already booked!!!  Please check room status!!!"})
+                        }
+            
+                        })
+                    
+               
+                     
+        }
+        else{
+            res.render("User_interface",{message2:"Room not found!!! PLease check room status!!!"});
+        }
+    })
+})
+
+app.post("/usercheckroom",(req,res)=>{
+        data.collection("room_data").findOne({seats:req.body.seats},function(err,result){
+            if (err) throw err;
+            var number1 = result.r_number;
+            var price1 = result.price;
+            if(result!=undefined){
+                data.collection("room_status").find({seats:req.body.seats},{ projection: { _id: 0, r_number: 1, date: 1, price: 1 } }).toArray(function(err,result){
+                    if (err) throw err;
+                    if(result.length>0){
+                        var a=0;
+                        console.log(result);
+                       for(let i=0;i<result.length;i++){
+                           
+                           if(result[i].date!=req.body.date){
+                               var number = result[i].r_number;
+                               a=1;
+                                res.render("User_interface",{message1:"Room number "+number+" is available and is priced at "+result[i].price});
+                           }
+                           
+                       }
+                       if(a==0){
+                           console.log("cannot find 1");
+                        res.render("User_interface",{message2:"Cannot find matching room number"});
+                       }
+                    }
+                    else{
+                        console.log("Undefined");
+                        res.render("User_interface",{message1:"Room number "+number1+" is available and is priced at "+price1});
+                    }
+                })
+            }
+            else{
+                console.log("cannot find 2");
+                        res.render("User_interface",{message2:"Cannot find matching room number"});
+            }
+        })
+})
+/////////////////////////////////////////////////////////////////////
+
+app.post("/sendfeedback",(req,res)=>{
+    data.collection("Feedback").insertOne({message:req.body.message},function(err,result){
+        if (err) throw err;
+        if(result!=undefined){
+            res.send("Thank You for your feedback!!!");
+        }
+    })
+})
+////////////////////////////////////////////////////////////////////
 app.get("/logout",(req,res)=>{
     res.render("index",{_message:"Logout Successfull"});
 })
+///////////////////////////////////////////////////////////
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port,()=>{
     console.log("Server started at port "+port)
 })
